712a013f31a6f78216559c24b5a2d0274a8ed959
Brian Sunter <brian@briansunter.com>
2019-12-29 21:56:48 -0800

    Updates

---
+++ precache-manifest.b9f8f01b8e3e0b1acb914356baa53c21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20019900dad2100e614c",
+    "url": "/bander/static/css/main.5a8cede0.chunk.css"
+  },
+  {
+    "revision": "20019900dad2100e614c",
+    "url": "/bander/static/js/main.20019900.chunk.js"
+  },
+  {
+    "revision": "502e314a71792817713e",
+    "url": "/bander/static/js/1.502e314a.chunk.js"
+  },
+  {
+    "revision": "981fa81c22087de13049",
+    "url": "/bander/static/js/runtime~main.981fa81c.js"
+  },
+  {
+    "revision": "442102f95112a851273f95846070d1f2",
+    "url": "/bander/index.html"
+  }
+];
