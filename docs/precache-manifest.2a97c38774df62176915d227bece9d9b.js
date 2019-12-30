b01c37b43da18aa5a4af3179e29512ffdc1a75a0
Brian Sunter <brian@briansunter.com>
2019-12-29 21:47:37 -0800

    docs folder

---
+++ docs/precache-manifest.2a97c38774df62176915d227bece9d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "272c9a738c068642993d",
+    "url": "/static/css/main.5a8cede0.chunk.css"
+  },
+  {
+    "revision": "272c9a738c068642993d",
+    "url": "/static/js/main.272c9a73.chunk.js"
+  },
+  {
+    "revision": "502e314a71792817713e",
+    "url": "/static/js/1.502e314a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f950e30a0a2bdb452d3db44d04192002",
+    "url": "/index.html"
+  }
+];
